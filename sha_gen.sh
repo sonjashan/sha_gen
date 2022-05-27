56157f047446009e2190874ddf2e2fc65a57befa
sonjashan <102500611+sonjashan@users.noreply.github.com>
2022-05-27 10:21:46 -0400

    get stuck at a = 5, b = 8, the while loop on 35, Walnut never writes the file

--- sha_gen.sh
+++ sha_gen.sh
@@ -12,27 +12,27 @@ rm -f ochem_gen_failed.txt
 recordIdx=()
 counter=0
 
-coproc java Main.Prover 
+coproc java Main.Prover
 
-for b in {2..10}
-do 
+for b in {2..100}
+do
 	for (( a=1; a<b; a++ ))
 	do
 		if (( a * 2 == b )); then
 			continue
 		fi
-		
+
 		echo "a,b = $a, $b"
-		
-		found=0		
+
+		found=0
 		for i in "${recordIdx[@]}";
 		do
 			echo "eval pp${a}_${b}_checkS${i} \"An (S${i}[n]=S${i}[n+${a}]|S${i}[n]=S${i}[n+${b}])\":\r" >&"${COPROC[1]}"
 
-			1>>waitforjava.txt 2>&1 cat "../Result/pp${a}_${b}_checkS${i}.txt" 
+			1>>waitforjava.txt 2>&1 cat "../Result/pp${a}_${b}_checkS${i}.txt"
 			while [ $? -eq 1 ]
 			do
-				1>>waitforjava.txt 2>&1 cat "../Result/pp${a}_${b}_checkS${i}.txt" 
+				1>>waitforjava.txt 2>&1 cat "../Result/pp${a}_${b}_checkS${i}.txt"
 			done
 
 			ppCheck=$(<"../Result/pp${a}_${b}_checkS${i}.txt")
@@ -42,9 +42,9 @@ do
 				echo "see pp${a}_${b}_checkS${i}.txt in Results"
 				found=1
 				break
-			fi			
+			fi
 		done
-		
+
 		if (( found == 0 ))
 		then
 			echo "running a=${a}, b=${b}" >> ./genOut/genlog.txt
@@ -57,19 +57,19 @@ EOF
 			1>>waitforjava.txt 2>&1 cat "../Result/cubeplusfree_S${counter}.txt"
 			while [ $? -eq 1 ]
 			do
-				1>>waitforjava.txt 2>&1 cat "../Result/cubeplusfree_S${counter}.txt" 
+				1>>waitforjava.txt 2>&1 cat "../Result/cubeplusfree_S${counter}.txt"
 			done
 
 			ppNewSeqCheck=$(<"../Result/pp${a}_${b}_checkS${counter}.txt")
 			cfNewSeqCheck=$(<"../Result/cubeplusfree_S${counter}.txt")
-			
+
 			if [ "${ppNewSeqCheck}" != "true" ] || [ "${cfNewSeqCheck}" != "false" ]
 			then
 				echo "see ochem_gen_failed.txt"
 				echo "${a}_${b}_311_S${counter}.txt by Ochem program does not work!!!" >> ochem_gen_failed.txt
 			else
 				echo "see ${a}_${b}_311_S${counter}.txt in genOut"
-			fi			
+			fi
 			recordIdx+=("$counter")
 			(( counter++ ))
 		fi
